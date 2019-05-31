c9fef21c53a70470c546672634cf2157aa3e406f
Ken Domino <kenneth.domino@domemtech.com>
2019-05-30 21:49:21 -0400

    Clean up--moving around extension methods to appropriate files.

--- Runtime/StringExt.cs
+++ Runtime/StringExt.cs
@@ -11,5 +11,19 @@ namespace Runtime
             if (string.IsNullOrEmpty(value)) return value;
             return value.Length <= maxLength ? value : value.Substring(0, maxLength);
         }
+
+        public static string provide_escapes(this string s)
+        {
+            StringBuilder new_s = new StringBuilder();
+            for (var i = 0; i != s.Length; ++i)
+            {
+                if (s[i] == '"' || s[i] == '\\')
+                {
+                    new_s.Append('\\');
+                }
+                new_s.Append(s[i]);
+            }
+            return new_s.ToString();
+        }
     }
 }
